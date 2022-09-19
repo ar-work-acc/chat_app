19f8a727b6ca85c618b111f4cd3aa924d69efb56
Louis Huang <ar.work.acc@gmail.com>
2022-09-19 12:49:50 +0800

    Update Docker compose settings.

--- run_docker_compose.sh
+++ run_docker_compose.sh
@@ -2,7 +2,13 @@
 docker compose down
 docker image prune -f
 docker volume prune -f
+docker network prune -f
 
+# building React code
+npm install --prefix app
+npm run build --prefix app
+
+# Docker compose up
 docker compose up -d --build
 docker container ls -a
 
